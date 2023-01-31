a816b83fdc8b403949837cef8ae7d6e81172b22b
Kiran Ikram <kiran.ikram91@gmail.com>
2023-01-31 12:16:02 +0000

    leetcode

--- python-prac/leetcode.py
+++ python-prac/leetcode.py
@@ -28,3 +28,49 @@ def subarray(arr,n,sum_):
       i += 1
 
     return 0
+
+
+def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        new = sorted(nums)
+        counter = 0
+        ids = []
+        for i in new:
+            if i == target:
+                ids.append(new.index(i) + counter)
+                counter += 1
+
+        return ids
+
+def reverseWords(self, s: str) -> str:
+        def reverse_array(arr):
+            start , end = 0 , len(arr)-1
+            while start < end:
+                arr[start], arr[end] = arr[end] , arr[start]
+                start += 1
+                end -= 1
+
+            return(arr)
+        s = s + " "
+        words = []
+        start = 0
+        end = 0
+        for i in range(len(s)):
+            if s[i] ==  " ":
+                word = s[start:i]
+    
+                words.append(word)
+                start = i+1
+
+
+
+        new_words = reverse_array(words)
+
+
+        new_s = ""
+        for word in new_words:
+            new_s = new_s + word
+            new_s = new_s + " "
+
+        new_s = new_s[:-1]
+
+        return new_s
